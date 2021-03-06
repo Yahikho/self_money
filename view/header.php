cdb7562bd160de6f1ad68507b743de5348576b13
yahiko <jhonbermudezpa98@gmail.com>
2021-03-05 21:53:39 -0500

    Estilos heade y crecion del edit profile

--- view/header.php
+++ view/header.php
@@ -9,11 +9,14 @@
 <?php
     if(isset($_SESSION['user_name'])){
 ?>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet"> 
 <link rel="stylesheet" href="../css/header.css">
 <header>
 <form action="" method="POST" class="form_header" >
-    <p><span><?=$_SESSION['user_name']?></span></p>
-    <button type="submit" name="signOut">Sign Out</button>
+        <a class="back-home" href="home.php">Home</a>
+        <button type="submit" name="signOut">Sign Out</button>
+        <p class="user-name"><a href="edit_profile.php"><?=$_SESSION['user_name']?></a></p>
 </form>
 </header>
 <?php
